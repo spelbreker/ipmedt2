7bb59df2acc5fb8dbcabc391f038e12b6cb16049
Patrick Brouwer <patrickbrouwer@outlook.com>
2015-09-24 12:52:19 +0200

    init project

---
+++ README.md
@@ -0,0 +1,21 @@
+# gulp basic build enviorment
+
+## build less and javascript
+
+
+# install
+
+- download node.js (https://nodejs.org/en/)
+- install node.js
+- in the project folder open you command promt or console
+- type : npm install 
+
+
+# usages commands
+
+ - gulp build (build 1 time)
+ - gulp (waits for changes and compile automatic)
+ - gulp squish-libs (concat and minimalize js libories)
+ - gulp build-js (validate and minimalize javascript)
+ - gulp build-css (validate and minimalize css)
+ - gulp watch (waits for changes and compile automatic)
---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+// Load Gulp
+var gulp    = require('gulp'),
+    gutil   = require('gulp-util');
+    plugins = require('gulp-load-plugins')();
+var sourcemaps = require('gulp-sourcemaps');
+
+// Start Watching: Run "gulp"
+gulp.task('default', ['watch']);
+
+// Minify jQuery: Run manually with: "gulp squish-jquery"
+gulp.task('squish-libs', function() {
+  return gulp.src('assets/js/libs/**/*.js')
+	
+    .pipe(plugins.uglify())
+    .pipe(plugins.concat('plugins.min.js'))
+	
+    .pipe(gulp.dest('build/js'));
+});
+
+// Minify Custom JS: Run manually with: "gulp build-js"
+gulp.task('build-js', function() {
+  return gulp.src('assets/js/*.js')
+	.pipe(sourcemaps.init())
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter('jshint-stylish'))
+    .pipe(plugins.uglify())
+    .pipe(plugins.concat('scripts.min.js'))
+	.pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('build/js'));
+});
+
+// Less to CSS: Run manually with: "gulp build-css"
+gulp.task('build-css', function() {
+    return gulp.src('assets/less/app.less')
+		.pipe(sourcemaps.init())
+        .pipe(plugins.plumber())
+        .pipe(plugins.less())
+        .on('error', function (err) {
+            gutil.log(err);
+            this.emit('end');
+        })
+        .pipe(plugins.autoprefixer(
+            {
+                browsers: [
+                    '> 1%',
+                    'last 2 versions',
+                    'firefox >= 4',
+                    'safari 7',
+                    'safari 8',
+                    'IE 8',
+                    'IE 9',
+                    'IE 10',
+                    'IE 11'
+                ],
+                cascade: false
+            }
+        ))
+        //.pipe(plugins.cssmin())
+		.pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('build/css')).on('error', gutil.log);
+});
+
+// Default task
+gulp.task('watch', function() {
+    gulp.watch('assets/js/libs/**/*.js', ['squish-libs']);
+    gulp.watch('assets/js/*.js', ['build-js']);
+    gulp.watch('assets/less/**/*.less', ['build-css']);
+});
+
+// Default task
+gulp.task('build', ['squish-libs','build-js','build-css']);
