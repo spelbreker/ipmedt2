9e9e0549c5edb22fcf8bfceb0ec5b95550a275c2
Patrick Brouwer <patrickbrouwer@outlook.com>
2015-09-25 10:31:33 +0200

    chart js toegevoegd. grid responsive fonts less

--- assets/js/fullpage-config.js
+++ assets/js/fullpage-config.js
@@ -2,7 +2,7 @@
 //full page js configuration
 $(document).ready(function() {
     $('#fullpage').fullpage({
-
+        sectionsColor: ['', '#4BBFC3', '#7BAABE', 'whitesmoke', '#F5F5F5','#4BBFC3', '#7BAABE'],
     });
 
 });
