468ad1ebb8c9947f21206494ebb37f022af461d4
Patrick Brouwer <patrickbrouwer@outlook.com>
2015-09-30 14:23:40 +0200

    + bug fixing

--- assets/js/storyline.js
+++ assets/js/storyline.js
@@ -4,24 +4,31 @@
 
 //first page
 $('#startReading').click(function(e){
+
+    //play sound
     $('#ModemSound').get(0).play();
 
-
+    //show connection animation
     $('#landing-txt,#startReading').hide();
     $('.connecting').show();
 
-    $("#ModemSound").bind('ended', function(){
+
+    $("#ModemSound").bind('ended', function()
+    { //if sound end
+
         //enable scroll and arrow keys
         $.fn.fullpage.setMouseWheelScrolling(true);
         $.fn.fullpage.setAllowScrolling(true);
+        $.fn.fullpage.setKeyboardScrolling(true);
+
         //move section down
         $.fn.fullpage.moveSectionDown();
 
+        //after 2 seconds set old text back
         setTimeout(function(){
             $('#landing-txt,#startReading').show();
             $('.connecting').hide();
         }, 2000);
-
 
     });
 });
