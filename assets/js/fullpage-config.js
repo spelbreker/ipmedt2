fca38bbecee49394a7cce30db40c12a19e9922b7
Patrick Brouwer <patrickbrouwer@outlook.com>
2015-10-09 14:27:29 +0200

    + highchart update for background + responsive fix + little changes

--- assets/js/fullpage-config.js
+++ assets/js/fullpage-config.js
@@ -3,37 +3,82 @@
 
 var firstVisit = true;
 
+mobilefullpage = {
+    sectionsColor: ['','#ebebe8','#ebebe8','#ebebe8','#ebebe8','#ebebe8','#ebebe8'],
+    anchors:['landing', 'apranet','video','websites','grafiek','wireless','timeline'],
+    menu: 'nav',
+    css3: true,
+    autoScrolling: false,
+    fitToSection: false,
+    animateAnchor: true,
+    scrollingSpeed: 70000000000,
+};
+
+normalfullpage = {
+    sectionsColor: ['','#ebebe8','#ebebe8','#ebebe8','#ebebe8','#ebebe8','#ebebe8'],
+    anchors:['landing', 'apranet','video','websites','grafiek','wireless','timeline'],
+    menu: 'nav',
+    css3: true,
+
+    afterLoad: function(anchorLink, index){
+        var loadedSection = $(this);
+        console.log('active');
+        //using index
+        if(index == 1){
+            if(firstVisit) {
+                $.fn.fullpage.setMouseWheelScrolling(false);
+                $.fn.fullpage.setAllowScrolling(false);
+                $.fn.fullpage.setKeyboardScrolling(false);
+            }
+        } else {
+            firstVisit = false;
+            $.fn.fullpage.setMouseWheelScrolling(true);
+            $.fn.fullpage.setAllowScrolling(true);
+            $.fn.fullpage.setKeyboardScrolling(true);
+        }
+
+        if(index == 3) {
+            $("#apranet-vid")[0].src += "&autoplay=1";
+        } else {
+            //player.stopVideo();
+        }
+    }
+};
+
+
 $(function () {
     $(document).ready(function() {
-        $('#fullpage').fullpage({
-            sectionsColor: ['','#ebebe8','#ebebe8','#ebebe8','#ebebe8','#ebebe8','#ebebe8'],
-            anchors:['landing', 'apranet','tcp-ip','websites','grafiek','wireless','timeline'],
-            menu: 'nav',
-            css3: true,
 
-            afterLoad: function(anchorLink, index){
-                var loadedSection = $(this);
+        function loadFullpageConfig() {
+            //load config
+            if($(window).width() < 900 ) {
+                console.log('responive');
+                $('#fullpage').fullpage(mobilefullpage);
 
-                //using index
-                if(index == 1){
-                    if(firstVisit) {
-                        $.fn.fullpage.setMouseWheelScrolling(false);
-                        $.fn.fullpage.setAllowScrolling(false);
-                        $.fn.fullpage.setKeyboardScrolling(false);
-                    }
-                } else {
-                    firstVisit = false;
-                    $.fn.fullpage.setMouseWheelScrolling(true);
-                    $.fn.fullpage.setAllowScrolling(true);
-                    $.fn.fullpage.setKeyboardScrolling(true);
-                }
+            } else  {
 
-                if(index == 3) {
-                    $("#apranet-vid")[0].src += "&autoplay=1";
-                } else {
-                    //player.stopVideo();
-                }
+                //$.fn.fullpage.destroy('all');
+                $('#fullpage').fullpage(normalfullpage);
+            }
+
+        }
+
+
+
+        $(window).resize(function () {
+            if($(window).width() < 900 ) {
+                $.fn.fullpage.autoScrolling(false);
+                $.fn.fullpage.fitToSection(false);
+                $.fn.fullpage.scrollingSpeed(70000000000);
+            } else  {
+                $.fn.fullpage.autoScrolling(true);
+                $.fn.fullpage.fitToSection(true);
+                $.fn.fullpage.scrollingSpeed(700);
             }
         });
+
+
+        //init config
+        loadFullpageConfig();
     });
 });
